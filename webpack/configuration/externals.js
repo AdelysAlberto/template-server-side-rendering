fba1c113b4bb667703db6a1cf4738f81126a3703
Adelys Belen <adalbeca@gmail.com>
2019-06-07 20:08:03 -0300

    upload projects

---
+++ webpack/configuration/externals.js
@@ -0,0 +1,8 @@
+// DevDependencies
+import nodeExternals from 'webpack-node-externals';
+
+export default () => [
+    nodeExternals({
+        whitelist: [ /^redux\/(store|modules)/ ],
+    }),
+];
